588c4e743ffe056c59f7690f7b61c6df67e4399a
Denis Trifan <trifan.denis1999@gmail.com>
2024-04-24 08:28:48 +0300

    Added LAB3 initial files

---
+++ IA_LAB3/main.py
@@ -0,0 +1,18 @@
+# code taken from https://github.com/apostolisv/chess-ai
+import graphics
+from Board import *
+
+
+if __name__ == '__main__':
+    keep_playing = True
+
+    board = Board(game_mode=0, ai=True, depth=1, log=True)  # game_mode == 0: whites down / 1: blacks down
+
+    while keep_playing:
+        graphics.initialize()
+        board.place_pieces()
+        graphics.draw_background(board)
+        keep_playing = graphics.start(board)
+        
+
+    
